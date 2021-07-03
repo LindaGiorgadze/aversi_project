b07bb17ecb69b1457429b3f108ba571724e49cfc
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-03 21:18:24 +0400

    update

--- js/script.js
+++ js/script.js
@@ -4,3 +4,38 @@ $("#burger-menu").on("click", function(){
   })
 
   // slider
+  let slidePosition = 0;
+  const slides = document.getElementsByClassName("carousel__item");
+  const totalSlides = slides.length;
+  
+document.getElementById("carousel__btn--next")
+  .addEventListener("click", function(){
+      moveToNextSlide();
+  });
+  document.getElementById("carousel__btn--prev")
+  .addEventListener("click", function(){
+      moveToPrevSlide();
+});
+function updateSlidesPosition(){
+  for(let slide of slides){
+    slide.classList.remove("carousel__item--visible");
+      slide.classList.add("carousel__item--hidden");
+  }
+    slides[slidePosition].classList.add("carousel__item--visible");
+}
+function moveToNextSlide(){   
+  if (slidePosition === totalSlides -1){
+    slidePosition = 0;
+  }else {
+    slidePosition++;
+  }
+  updateSlidesPosition();
+}
+function moveToPrevSlide(){
+  if (slidePosition === 0){
+      slidePosition = 0;  
+  } else {
+    slidePosition--;
+  }
+  updateSlidesPosition();
+}

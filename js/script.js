92e311e9724eb52dad03034532d8436aafeec2b0
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-04 17:46:23 +0400

    update

--- js/script.js
+++ js/script.js
@@ -53,3 +53,33 @@ function myFunction4() {document.getElementById('heart5').style.color = 'red';}
 function myFunction5() {document.getElementById('heart6').style.color = 'red';}
 function myFunction6() {document.getElementById('heart7').style.color = 'red';}
 function myFunction7() {document.getElementById('heart8').style.color = 'red';}
+
+// slider2
+const gap = 16;
+
+const carousel = document.getElementById("carousel2"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));

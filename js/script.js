77058506178aaf78c0886e0bf524ad716a85918e
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-10 18:52:12 +0400

    added carousel

--- js/script.js
+++ js/script.js
@@ -4,80 +4,45 @@ $("#burger-menu").on("click", function(){
   })
 
   // slider
-  $(function(){
-    $('.multiple-items').slick({
-   infinite: true,
-   slidesToShow: 3,
-   slidesToScroll: 1,
-   arrows: true,
-   dots:false,
-   responsive: [
-       {
-         breakpoint: 768,
-         settings: {
-           slidesToShow: 2,
-           slidesToScroll: 1
-         }
-       },
-       {
-         breakpoint: 480,
-         settings: {
-           slidesToShow: 1,
-           slidesToScroll: 1
-         }
-       }
-     ]
-   });    
+  let slidePosition = 0;
+  const slides = document.getElementsByClassName("carousel__item");
+  const totalSlides = slides.length;
+  
+  document.getElementById("carousel__btn--next")
+  .addEventListener("click", function(){
+      moveToNextSlide();
   });
+  document.getElementById("carousel__btn--prev")
+  .addEventListener("click", function(){
+      moveToPrevSlide();
+  });
+  function updateSlidesPosition(){
+      for(let slide of slides){
+          slide.classList.remove("carousel__item--visible");
+          slide.classList.add("carousel__item--hidden");
+      }
+      slides[slidePosition].classList.add("carousel__item--visible");
+  }
+  function moveToNextSlide(){ 
+      if (slidePosition === totalSlides -1){
+          slidePosition = 0;
+      } else {
+          slidePosition++;
+      }
+      updateSlidesPosition();
+  }
+  function moveToPrevSlide(){
+      if (slidePosition === 0){
+      // totalSlides -1, if you wan't infinite carousel effect to both ways
+          slidePosition = 0;  
+      } else {
+          slidePosition--;
+      }
+      updateSlidesPosition();
+  }
 
 // slider2
-const selection = document.querySelectorAll(".box span"),
-showItem = document.querySelectorAll(".price__box"),
-arrowMove =document.querySelector(".arrow"),
-add = document.querySelector(".add"),
-minus = document.querySelector(".minus");
-let currValue = 1;
-selection.forEach((item) =>{
-    item.addEventListener('click',showdata);
-});
-function showdata(){
-    reset();
-    this.classList.add('active');
-    const dataIndex = Number(this.dataset.index);
-    showItem.forEach((item,index) => {
-        if(dataIndex == index){
-            if(index == 0){
-                arrowMove.style.left = "35px"; 
-            }
-            if(index == 1){
-                arrowMove.style.left = "150px"; 
-            }
-            if(index == 2){
-                arrowMove.style.left = "265px"; 
-            }
-            item.classList.add("show");
-        }
-    });
-}
-function reset(){
-    selection.forEach((item) => {
-        item.classList.remove("active")
-    });
-    showItem.forEach((item) => {
-        item.classList.remove("show")
-    });
-}
-add.addEventListener('click',() => {
-    currValue++;
-    document.querySelector(".adding span").innerHTML = currValue;
-});
-minus.addEventListener('click',() => {
-    currValue--;
-    if(currValue <= 0){
-        currValue = 0;
-    }
-    document.querySelector(".adding span").innerHTML = currValue;
-});
+
 
 // heart
 function func()

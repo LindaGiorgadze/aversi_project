dc6a092f30d234232e8a794981e4a9cea4901b9d
Gvantsa19 <oqropiridzegvantsa@gmail.com>
2021-07-18 18:15:13 +0400

    preview

---
+++ js/preview-photos.js
@@ -0,0 +1,16 @@
+const productPreview = document.querySelector('.product_preview');
+        const leftSide = document.querySelector('.left-side');
+        const images = document.querySelectorAll('img');
+        console.log(images)
+        const rightSide = document.createElement('div');
+        productPreview.appendChild(rightSide);
+        rightSide.classList.add('right-side');
+        rightSide.style.cssText+= `background-image: url('./images/back.png'); margin-top: 50px;`;
+
+        images.forEach(image => {
+            image.addEventListener('click', function() {
+                let src = image.src;
+                rightSide.style.cssText+= `background-image: url(${src})`;
+                
+            })
+        });
